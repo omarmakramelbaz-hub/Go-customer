9194db1b59aaa64dcf233b693ddeff7a89746782
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-19 23:07:20 +0300

    Add web Routes Library bridge

---
+++ lib/view/layout/map/utils/routes_client_bridge_stub.dart
@@ -0,0 +1,8 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+Future<List<LatLng>?> getWebRoutePath({
+  required LatLng origin,
+  required LatLng destination,
+}) async {
+  return null;
+}
