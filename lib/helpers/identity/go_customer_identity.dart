b030cdc197a744428d0251df5d38378e552f228f
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-25 21:43:20 +0300

    Add shared GO customer identity tokens

---
+++ lib/helpers/identity/go_customer_identity.dart
@@ -0,0 +1,5 @@
+abstract final class GoCustomerIdentity {
+  static const displayName = 'GO';
+  static const logoAsset = 'assets/svg/go_logo.svg';
+  static const lightLogoAsset = 'assets/svg/go_logo_light.svg';
+}
