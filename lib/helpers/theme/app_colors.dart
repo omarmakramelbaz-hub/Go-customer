02bf32f210a1773e97fc6c3ac4b0b8250399c607
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-25 21:43:33 +0300

    Align Go Customer palette with Go Partner identity

--- lib/helpers/theme/app_colors.dart
+++ lib/helpers/theme/app_colors.dart
@@ -2,36 +2,36 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color mainAppColor = const Color(0xffFD7201);
-  static Color lightMainAppColor = const Color(0xffFFEBBC);
+  static Color lightMainAppColor = const Color(0xffFFF1E5);
   static Color darkMainAppColor = const Color(0xffF66E00);
-  static Color secondAppColor = const Color(0xff000000);
-  static Color borderColor = const Color(0xffDBDBDB);
-  static Color splachScreenColor = const Color(0xffF1F1F1);
+  static Color secondAppColor = const Color(0xff171A1F);
+  static Color borderColor = const Color(0xffDADDE2);
+  static Color splachScreenColor = const Color(0xffF8F9FB);
   static Color boxHomeColor = const Color(0xffF8F5F5);
   static Color scaffoldColor = const Color(0xffFFFFFF);
   static Color hintColor = const Color(0xffAEAEAE);
-  static Color darkTextColor = const Color(0xff191C24);
-  static Color lightTextColor = const Color(0xffB5B5B5);
-  static Color greyColor = const Color(0xff818181);
+  static Color darkTextColor = const Color(0xff171A1F);
+  static Color lightTextColor = const Color(0xff7D8490);
+  static Color greyColor = const Color(0xff9AA0AA);
   static Color greenColor = const Color(0xff05D883);
   static Color yellowColor = const Color(0xffFFC643);
   static Color lightGreyColor = const Color(0xffF6F6F6);
-  static Color lightDarkColor = const Color(0xff323943);
-  static Color darkGreyColor = const Color(0xff727272);
+  static Color lightDarkColor = const Color(0xff171A1F);
+  static Color darkGreyColor = const Color(0xff6D7480);
   static Color whiteColor = const Color(0xffffffff);
   static Color offWhiteColor = const Color(0xffF4F5F7);
   static Color textFormFillColor = const Color(0xFFFFFFFF);
-  static Color textFormBorderColor = const Color(0xffDBDBDB);
+  static Color textFormBorderColor = const Color(0xffDADDE2);
 
-  static Color appBarColor = const Color(0xffFFDE92);
+  static Color appBarColor = const Color(0xff171A1F);
   static Color appBarColor1 = const Color(0xffFD7201);
   static Color appBarColor2 = const Color(0xffFF9D4D);
   static Color buttonTextColor = const Color(0xffFFFFFF);
 
   static Color redColor = const Color(0xffFF0000);
-  static Color blackColor = const Color(0xff000000);
+  static Color blackColor = const Color(0xff171A1F);
 
-  static Color textFormColor = const Color(0xff000000);
+  static Color textFormColor = const Color(0xff171A1F);
   static Color popupColor = const Color(0xffffffff);
   static Color appBarTextColor = const Color(0xffFFFFFF);
   static Color inActiveColor = const Color(0xFFC4C5CE);
